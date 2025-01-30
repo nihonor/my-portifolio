402f0133e6308f9d40278075893f624df5724ab9
nihonor <niyitanganihonor@gmail.com>
2025-01-30 22:08:19 +0200

    refining sidebar

--- app/AppSidebar.tsx
+++ app/AppSidebar.tsx
@@ -1,5 +1,21 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
-
+import {
+  Calendar,
+  Linkedin,
+  Github,
+  FileUser,
+  BookText,
+  MessageSquareText,
+  Home,
+  Inbox,
+  Search,
+  Settings,
+  Zap,
+  BriefcaseBusiness,
+} from "lucide-react";
+import Image from "next/image";
+import profilePic from "../public/honor.png"
+import { CiLinkedin } from "react-icons/ci";
+import { FaWhatsapp } from "react-icons/fa";
 import {
   Sidebar,
   SidebarContent,
@@ -10,33 +26,52 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { title } from "process";
 
 // Menu items.
 const items = [
   {
     title: "Home",
     url: "#",
-    icon: Home,
+    icon: Zap,
   },
   {
-    title: "Inbox",
+    title: "About",
     url: "/about",
-    icon: Inbox,
+    icon: MessageSquareText,
   },
   {
-    title: "Calendar",
+    title: "Projects",
     url: "#",
-    icon: Calendar,
+    icon: BriefcaseBusiness,
   },
   {
-    title: "Search",
+    title: "Certificates",
     url: "#",
-    icon: Search,
+    icon: BookText,
   },
   {
-    title: "Settings",
+    title: "Resume",
     url: "#",
-    icon: Settings,
+    icon: FileUser,
+  },
+];
+
+const socials = [
+  {
+    title: "LinkedIn",
+    url: "#",
+    icon: CiLinkedin,
+  },
+  {
+    title: "Github",
+    url: "#",
+    icon: Github,
+  },
+  {
+    title: "WhatsApp",
+    url: "#",
+    icon: FaWhatsapp,
   },
 ];
 
@@ -45,10 +80,37 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel className="my-8 ">
+            {/* <div><img src="" alt="Honore image"/></div> */}
+            <Image src={profilePic} alt="Honore image"width={45} className="rounded-full object-cover"/>
+            <div ><h2 className="font-bold text-sm text-gray-600 ">N.Honore</h2>
+            <p className="text-[13px]">Software developer & Cyber expert</p>
+            </div>
+          </SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="font-bold text-sm font text-gray-600">
+            Socials
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {socials.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
                     <a href={item.url}>
